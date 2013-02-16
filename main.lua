0f5518a2d6749c273405d06830209ef4d1896206
Scott Feeney <scott@oceanbase.org>
2013-02-16 16:37:42 -0500

    Unfinished LuaJIT FFI based driver, never terminates

--- main.lua
+++ main.lua
@@ -24,6 +24,30 @@ for i=3,#arg,2 do
     effect[knobName] = arg[i]
 end
 
+
+-- Use LuaJIT's FFI to handle reading and writing floats.
+local ffi = require "ffi"
+-- Wait, how am I gonna do this?
+-- I don't think I can... does the FFI auto convert Lua filehandles
+-- to FILE * and vice versa? Probably not.
+-- Though, I can define a sample pair. Let's try it.
+ffi.cdef[[
+typedef struct { float f[2]; } sample_pair;
+size_t fread(void *ptr, size_t size, size_t nmemb, void *stream);
+size_t fwrite(const void *ptr, size_t size, size_t nmemb, void *stream);
+]]
+
+local samplePair = ffi.new("sample_pair[?]", 1)
+local plainSamples = {}
+while ffi.C.fread(samplePair, 8, 1, io.stdin) do
+    plainSamples[1] = samplePair[0].f[0]
+    plainSamples[2] = samplePair[0].f[1]
+    effect:process(plainSamples)
+    samplePair[0].f[0] = plainSamples[1]
+    samplePair[0].f[1] = plainSamples[2]
+    if not ffi.C.fwrite(samplePair, 8, 1, io.stdout) then break end
+end
+
 -- It turns out reading and writing binary data (e.g. floats)
 -- is annoying in Lua, requiring an add-on C library.
 --
