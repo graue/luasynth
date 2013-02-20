3b449b44aa21c37496475772df7f049376d65c48
Scott Feeney <scott@oceanbase.org>
2013-02-20 11:22:40 -0500

    Filter now sets coefficients, still unfinished

--- effects/filter.lua
+++ effects/filter.lua
@@ -1,10 +1,50 @@
 -- filter: highpass, lowpass, bandpass, notch
+--
+-- Implemented with formulas from Robert Bristow-Johnson's
+-- Audio EQ Cookbook.
 
 local wrap = require "wrap"
 
 
 local function updateCoefs(state)
-    -- TODO
+    local Fs = 44100  -- XXX: need interface to set/get a custom sample rate
+    local f0 = state.public.center  -- Cutoff or center frequency
+    local Q  = state.public.q
+
+    local w0 = 2*math.pi*f0/Fs
+    local cos_w0 = math.cos(w0)
+    local sin_w0 = math.sin(w0)
+    local alpha = sin_w0/(2*Q)
+
+    if state.public.filttype == 'Lowpass' then
+        state.b0 =  (1 - cos_w0)/2
+        state.b1 =   1 - cos_w0
+        state.b2 =  (1 - cos_w0)/2
+        state.a0 =   1 + alpha
+        state.a1 =  -2*cos_w0
+        state.a2 =   1 - alpha
+    elseif state.public.filttype == 'Highpass' then
+        state.b0 =  (1 + cos_w0)/2
+        state.b1 = -(1 + cos_w0)
+        state.b2 =  (1 + cos_w0)/2
+        state.a0 =   1 + alpha
+        state.a1 =  -2*cos_w0
+        state.a2 =   1 - alpha
+    elseif state.public.filttype == 'Bandpass' then
+        state.b0 =   sin_w0/2
+        state.b1 =   0
+        state.b2 =  -sin_w0/2
+        state.a0 =   1 + alpha
+        state.a1 =  -2*cos_w0
+        state.a2 =   1 - alpha
+    elseif state.public.filttype == 'Notch' then
+        state.b0 =   1
+        state.b1 =  -2*cos_w0
+        state.b2 =   1
+        state.a0 =   1 + alpha
+        state.a1 =  -2*cos_w0
+        state.a2 =   1 - alpha
+    end
 end
 
 
