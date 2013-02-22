c9f644b6085dcd839ce80da6ff24bd464bf9460c
Scott Feeney <scott@oceanbase.org>
2013-02-21 22:01:35 -0500

    Finish HistArray implementation
    Beware of bugs in this code. I have only shown it passes the 9 tests I
    wrote, not tried to use it :)

--- histArray.lua
+++ histArray.lua
@@ -2,16 +2,45 @@
 
 local M = {}
 
+local function computeActualIndexFor(key, histSize)
+    return ((key-1) % (histSize+1) + 1)
+end
+
+local function getAllElementsFrom(proxy, private)
+    local results = {}
+    local earliestIndex = private.latestIndex - private.histSize
+
+    for i=earliestIndex,private.latestIndex do
+        results[i] = private[computeActualIndexFor(i, private.histSize)]
+    end
+    return results
+end
+
 local function lookupKeyIn(proxy, private, key)
-    -- XXX
     if type(key) == 'number' then
-        -- blah blah blah
+        if math.floor(key) ~= key then
+            error("attempt to access non-integer index in HistArray")
+        end
+
+        -- Check if index is valid
+        if key > private.latestIndex then
+            error("attempt to access too-new index in HistArray")
+        elseif key < private.latestIndex - private.histSize then
+            error("attempt to access too-old index in HistArray")
+        end
+
+        return private[computeActualIndexFor(key, private.histSize)]
     elseif key == 'histSize' then
         return private.histSize
+    elseif key == 'all' then
+        return private.all
+    elseif key == '_debug' then
+        return private  -- XXX
     else
-        return 'whatever'
+        error("attempt to access undefined HistArray key " .. tostring(key))
     end
 end
+
 
 local function setKeyIn(proxy, private, key, newVal)
     if type(key) ~= 'number' then
@@ -26,8 +55,18 @@ local function setKeyIn(proxy, private, key, newVal)
         error("attempt to set old index in HistArray")
     end
 
-    -- actually set it XXX
+    local previousHighIndex = private.latestIndex
+
+    private[computeActualIndexFor(key, private.histSize)] = newVal
+    private.latestIndex = key
+
+    -- Zero-fill if we advanced by more than one.
+    local backFillTo = math.max(key - private.histSize, previousHighIndex + 1)
+    for i=key-1,backFillTo,-1 do
+        private[computeActualIndexFor(i, private.histSize)] = 0
+    end
 end
+
 
 function M.new(histSize)
     if type(histSize) ~= 'number' or math.floor(histSize) ~= histSize then
@@ -49,9 +88,16 @@ function M.new(histSize)
     setmetatable(proxy, meta)
 
     private.histSize = histSize
-    -- XXX: more initialization
+    private.all = function(proxy)
+        return getAllElementsFrom(proxy, private)
+    end
+
+    -- Initialize array with zeroes.
+    for v = 1, histSize+1 do private[v] = 0 end
+    private.latestIndex = 0
 
     return proxy
 end
+
 
 return M
